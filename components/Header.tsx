d280ecb68b14974a90a322c89170f596208946f5
Khalil Tarabulsi <kh476379@dal.ca>
2023-01-06 12:44:55 -0400

    added framer-motion animation to header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,18 +1,45 @@
 import React from 'react'
 import { SocialIcon } from "react-social-icons"
+import { motion } from 'framer-motion'
 
 type Props = {}
 
 function Header({ }: Props) {
     return (
         <header className='sticky top-0 p-5 flex items-start justify-between max-w-7xl mx-auto z-30 xl:items-center'>
-            <div className='flex flex-row items-center'>
+            <motion.div className='flex flex-row items-center'
+                initial={{
+                    x: -500,
+                    opacity: 0,
+                    scale: 0.5
+                }}
+                animate={{
+                    x: 0,
+                    opacity: 1,
+                    scale: 1
+                }}
+                transition={{
+                    duration: 1.5,
+                }}>
                 <SocialIcon bgColor='transparent' fgColor='gray' url="https://www.linkedin.com/in/khalil-tarabulsi/" />
-            </div>
-            <div className='flex flex-row items-center text-gray-300 cursor-pointer'>
+            </motion.div>
+            <motion.div className='flex flex-row items-center text-gray-300 cursor-pointer'
+                initial={{
+                    x: 500,
+                    opacity: 0,
+                    scale: 0.5
+                }}
+                animate={{
+                    x: 0,
+                    opacity: 1,
+                    scale: 1
+                }}
+                transition={{
+                    duration: 2.5,
+                }}>
                 <SocialIcon className='cursor-pointer' bgColor='transparent' fgColor='gray' network='email' />
                 <p className='uppercase hidden md:inline-flex text-xs text-gray-500'>get in touch</p>
-            </div></header>
+            </motion.div></header>
     )
 }
 
