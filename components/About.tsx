ae4ed065285ca95a1b1e1f0b3985e3c5e517229f
Khalil Tarabulsi <kh476379@dal.ca>
2023-01-12 13:06:18 -0400

    clean ups in About me section

--- components/About.tsx
+++ components/About.tsx
@@ -30,21 +30,18 @@ function About({ }: Props) {
 
             <div className="max-h-20 md:max-h-80 lg:max-h-80 text-sm relative -mt-20 lg:mb-0  space-y-10 px-0 md:px-10">
                 <h4 className="lg:text-3xl text-4xl font-semibold">Here is a little background</h4>
-
-                <p className="bg-slate-100 p-1 rounded text-slate-500 ">
+                <p className="bg-slate-100 text-slate-500 font-serif">
                     ❝ Meet Khalil, the fun-loving full-stack engineer with a passion for building awesome web applications! With a background in deep learning, cloud engineering and a love for all things tech❞
                 </p>
-                <p className="p-1 rounded ">
-                    I specialize in building and deploying cutting-edge web applications using the latest technologies. <b>My weapon of choice?</b> <span className="text-[#03A9F4] font-medium rounded-lg bg-indigo-50 px-2">Python and its trusty sidekick, Flask.  </span> With them, I've developed robust and scalable Rest APIs that have served as the backbone of many successful projects. But I don't stop there, I love to play with the front-end too, I'm <b>well-versed in Javascript frameworks such as</b> <span className="text-[#03A9F4] font-medium rounded-lg bg-indigo-50 px-2">React and Vue.js</span>, and I use them to create dynamic and responsive user interfaces that provide seamless experiences. And when it comes to deployment, I don't play around, I <b> put my apps in the cloud</b>, <span className="text-[#03A9F4] font-medium rounded-lg bg-indigo-50 px-2">specifically AWS</span>, it allows me to take advantage of the scalability, reliability, and cost-effectiveness that it offers.
+                <p>
+                    I specialize in building and deploying cutting-edge web applications using the latest technologies. <b>My weapon of choice?</b> <span className="text-[#03A9F4] font-medium rounded-lg bg-indigo-50 px-2">Python and its trusty sidekick, Flask.  </span> With them, I've developed robust and scalable Rest APIs that have served as the backbone of many successful projects.
                 </p>
 
-                <p className="p-1 rounded ">
+                <p>But I don't stop there, I love to play with the front-end too, I'm <b>well-versed in Javascript frameworks such as</b> <span className="text-[#03A9F4] font-medium rounded-lg bg-indigo-50 px-2">React and Vue.js</span>, and I use them to create dynamic and responsive user interfaces that provide seamless experiences. And when it comes to deployment, I don't play around, I <b> put my apps in the cloud</b>, <span className="text-[#03A9F4] font-medium rounded-lg bg-indigo-50 px-2">specifically AWS</span>, it allows me to take advantage of the scalability, reliability, and cost-effectiveness that it offers.</p>
+                <p>
                     <b> When it comes to data, I'm a real whiz! </b> I have a deep understanding of data management, <span className="text-[#03A9F4] font-medium rounded-lg bg-indigo-50 px-2"> with skills in cleaning, manipulating, and analyzing large datasets</span> using powerful libraries such as Pandas and Numpy. I can turn data into insights and insights into action.
                 </p>
 
-                <p className="p-1 rounded ">
-                    So <b>if you're looking for a full-stack engineer</b> who is passionate, skilled, and fun, look no further! <span className="text-[#8BC34A] text-lg rounded-lg bg-indigo-50 px-3"> <Link href="#contact"> Hire me</Link> </span> and let's build something amazing together!
-                </p>
             </div>
         </div >
     )
