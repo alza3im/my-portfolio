c36e0d1b0e9d78be6fb6eaad3240b0f7fd786b8b
Khalil Tarabulsi <kh476379@dal.ca>
2023-01-17 00:19:58 -0400

    used react-hook-form

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -1,9 +1,23 @@
 import React from 'react'
-import { PhoneIcon, MapPinIcon, EnvelopeIcon } from '@heroicons/react/24/solid'
+import { MapPinIcon, EnvelopeIcon } from '@heroicons/react/24/solid'
+import { useForm, SubmitHandler } from "react-hook-form";
 
 type Props = {}
 
+type Inputs = {
+    name: string;
+    email: string;
+    subject: string;
+    message: string;
+}
+
 const Contact = (props: Props) => {
+    const { register, handleSubmit } = useForm<Inputs>();
+    const onSubmit: SubmitHandler<Inputs> = formData => {
+
+        window.location.href = `mailto:khaleel_097@hotmail.com?subject=${formData.subject}&body=${formData.message}`
+    }
+
     return (
         <div className='h-screen flex relative flex-col text-center md:text-left md:flex-row max-w-7xl px-10
         justify-evenly mx-auto items-center '>
@@ -29,14 +43,14 @@ const Contact = (props: Props) => {
                     </div>
                 </div>
 
-                <form className='flex flex-col space-y-2 w-fit mx-auto' action="">
+                <form onSubmit={handleSubmit(onSubmit)} className='flex flex-col space-y-2 w-fit mx-auto' action="">
                     <div className='flex space-x-2'>
-                        <input placeholder='Name' className='contactInput focus:border-slate-800  hover:border-[#03A9F4]/40' type="text" />
-                        <input placeholder='Email' className='contactInput focus:border-slate-800  hover:border-[#03A9F4]/40' type="email" />
+                        <input {...register('name')} placeholder='Name' className='contactInput focus:border-slate-800  hover:border-[#03A9F4]/40' type="text" />
+                        <input {...register('email')} placeholder='Email' className='contactInput focus:border-slate-800  hover:border-[#03A9F4]/40' type="email" />
                     </div>
-                    <input placeholder='Subject' className='contactInput focus:border-slate-800  hover:border-[#03A9F4]/40' type="text" />
+                    <input {...register('subject')} placeholder='Subject' className='contactInput focus:border-slate-800  hover:border-[#03A9F4]/40' type="text" />
 
-                    <textarea placeholder='Message' className='contactInput focus:border-slate-800  hover:border-[#03A9F4]/40' />
+                    <textarea {...register('message')} placeholder='Message' className='contactInput focus:border-slate-800  hover:border-[#03A9F4]/40' />
                     <button className='bg-[#03A9F4]/50 py-5 px-10 rounded-md text-black'>Submit</button>
                 </form>
 
