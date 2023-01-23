5d1aea2d2477fa7430172ad5761b48de48853347
Khalil Tarabulsi <kh476379@dal.ca>
2023-01-23 11:10:06 -0400

    prettier contact form

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -25,14 +25,12 @@ const Contact = (props: Props) => {
                 Contact Me
             </h3>
 
-            <div className="mt-20 flex flex-col space-y-10">
-                <h4 className=' text-2xl font-semibold text-center'>
+            <div className="mt-28 w-3/5 sm:w-1/2 flex flex-col space-y-10">
+                <h4 className=' text-1xl font-semibold  text-center'>
                     I have got exactly what you are looking for.
-                    <p><span className="text-[#03A9F4] font-medium rounded-lg bg-indigo-50 px-2">Let&apos;s chat</span></p>
                 </h4>
 
                 <div >
-
                     <div className="flex items-center space-x-5 justify-center">
                         <EnvelopeIcon className='text-[#03A9F4] h-5 w-5 animate-pulse' />
                         <p className='text-1xl text-left'>khaleel_097@hotmail.com</p>
@@ -41,22 +39,72 @@ const Contact = (props: Props) => {
                         <MapPinIcon className='text-[#03A9F4] h-5 w-5 animate-pulse' />
                         <p className='text-1xl'>Canada</p>
                     </div>
+                    {/* <div className="flex items-center space-x-5 justify-center">
+                        <p><span className="text-[#03A9F4] font-medium rounded-lg bg-indigo-50 px-2">Let&apos;s chat</span></p>
+                    </div> */}
                 </div>
 
-                <form onSubmit={handleSubmit(onSubmit)} className='flex flex-col space-y-2 w-fit mx-auto' action="">
-                    <div className='flex space-x-2'>
-                        <input {...register('name')} placeholder='Name' className='contactInput focus:border-slate-800  hover:border-[#03A9F4]/40' type="text" />
-                        <input {...register('email')} placeholder='Email' className='contactInput focus:border-slate-800  hover:border-[#03A9F4]/40' type="email" />
+                <form className="space-y-2 bg-white p-6 rounded-lg shadow-md" onSubmit={handleSubmit(onSubmit)}>
+                    <div className="mb-4">
+                        <label className="block text-gray-700 font-medium mb-2" htmlFor="name">
+                            Name
+                        </label>
+                        <input
+                            className="form-input focus:outline-none focus:shadow-outline"
+                            id="name"
+                            type="text"
+                            placeholder="John Doe"
+                            {...register('name')}
+                        />
                     </div>
-                    <input {...register('subject')} placeholder='Subject' className='contactInput focus:border-slate-800  hover:border-[#03A9F4]/40' type="text" />
-
-                    <textarea {...register('message')} placeholder='Message' className='contactInput focus:border-slate-800  hover:border-[#03A9F4]/40' />
-                    <button className='bg-[#03A9F4]/50 py-5 px-10 rounded-md text-black'>Submit</button>
+                    <div className="mb-4">
+                        <label className="block text-gray-700 font-medium mb-2" htmlFor="email">
+                            Email
+                        </label>
+                        <input
+                            className="form-input focus:outline-none focus:shadow-outline"
+                            id="email"
+                            type="email"
+                            placeholder="johndoe@example.com"
+                            {...register('email')}
+                        />
+                    </div>
+                    <div className="mb-4">
+                        <label className="block text-gray-700 font-medium mb-2" htmlFor="subject">
+                            Subject
+                        </label>
+                        <input
+                            className="form-input focus:outline-none focus:shadow-outline"
+                            id="subject"
+                            type="text"
+                            placeholder="johndoe@example.com"
+                            {...register('subject')}
+                        />
+                    </div>
+                    <div className="mb-4">
+                        <label className="block text-gray-700 font-medium mb-2" htmlFor="message">
+                            Message
+                        </label>
+                        <input
+                            className="form-input focus:outline-none focus:shadow-outline"
+                            id="message"
+                            type="text"
+                            placeholder="Hey Khalil, I am in love with your profile..."
+                            {...register('message')}
+                        />
+                    </div>
+                    <div className="text-center">
+                        <button
+                            className="bg-[#03A9F4]/50 text-white py-2 px-4 rounded-lg hover:bg-[#03A9F4]"
+                            type="submit"
+                        >
+                            Submit
+                        </button>
+                    </div>
                 </form>
-
             </div>
-
         </div>
+
     )
 }
 
